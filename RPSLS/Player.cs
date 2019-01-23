3f7f78334927e99ee53a57ce095f153547f259ac
unknown <pantazon-b@live.com>
2019-01-23 16:40:30 -0600

    Initial commit. Created class Game, Gestures, and Players. Creaetd Program but nothing is in it

---
+++ RPSLS/Player.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPSLS
+{
+    class Player
+    {
+        //member variables
+        public string name;
+        public int score;
+        public string usedGesture;
+        //constructor
+        public Player(string name, int score, string usedGesture)
+        {
+            this.name = name;
+            this.score = score;
+            this.usedGesture = usedGesture;
+
+        }
+        //member methods
+        public void makeGesture()
+        {
+            Console.WriteLine("Player " + name + "used " + usedGesture + "!");
+        }
+    }
+}
