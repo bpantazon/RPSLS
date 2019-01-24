042194d30b299d1e988bd2363e40cccdba78efc4
Ben Pantazon <pantazon-b@live.com>
2019-01-24 10:42:52 -0600

    Created child classes of HumanPlayer and AIPlayer with parent of Player. Began working on a new class named Game with methods getPlayers and displayRules.

--- RPSLS/Game.cs
+++ RPSLS/Game.cs
@@ -9,6 +9,46 @@ namespace RPSLS
     class Game
     {
         //member variables
-        //has rules, score calculator, executes turns
+        Player playerOne;
+        Player playerTwo;
+        public int numberOfPlayers;
+        public string playerOneName;
+        public string playerTwoName;
+        //constructor
+        public Game()
+        {
+
+        }
+        //member methods
+        public void getPlayers()
+        {
+            Console.WriteLine("How many players are there? Are there '1' or '2'?");
+            numberOfPlayers = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("What is the first player's name?");
+            playerOneName = Console.ReadLine();
+            Console.WriteLine("What is the second player's name?");
+            playerTwoName = Console.ReadLine();
+            if (numberOfPlayers == 1){
+                playerOne = new Human(playerOneName, 0);
+                playerTwo = new AI(playerTwoName, 0);
+            }
+            else if (numberOfPlayers == 2)
+            {
+                playerOne = new Human(playerOneName, 0);
+                playerTwo = new AI(playerTwoName, 0);
+            }
+            else
+            {
+                getPlayers();
+            }
+            
+
+        }
+
+        public void displayRules()
+        {
+            Console.WriteLine("Scissors cuts PaperPaper covers Rock, Rock crushes Lizard,Lizard poisons Spock, Spock smashes Scissors,");
+            Console.WriteLine("Scissors decapitates Lizard, Lizard eats Paper, Paper disproves Spock, Spock vaporizes Rock, and Rock crushes Scissors");
+        }
     }
 }
